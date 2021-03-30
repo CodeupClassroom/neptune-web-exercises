b3872eafd85135980752ef2619d9c1c1e678ee67
Javier Ruedas <javier@codeup.com>
2021-03-30 14:43:53 -0500

    refactor break-and-continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,37 @@
 "use strict";
+//var oddNumber = 0;
 
-var oddNumber = 0;
-
-while (oddNumber !== "You're never gonna come up with this"){
-    oddNumber = parseFloat(prompt("Please enter an odd number between 1 and 50."));
-    if (oddNumber % 2 === 1 || (oddNumber > 0 && oddNumber < 50)) {break;}
-}
+// while (oddNumber !== "You're never gonna come up with this"){
+//     oddNumber = parseFloat(prompt("Please enter an odd number between 1 and 50."));
+//     if (oddNumber % 2 === 1 || (oddNumber > 0 && oddNumber < 50)) {break;}
+// }
 // while (oddNumber % 2 !== 1 || !(oddNumber > 0 && oddNumber < 50)) {
 //     oddNumber = parseFloat(prompt("Please enter an odd number between 1 and 50."));
 //     //if (oddNumber % 2 === 1 && (oddNumber > 0 && oddNumber < 50)){break;}
 // }
+
+// var oddNumber = parseInt(prompt("Give me an odd number between 1 and 50 please?"));
+var oddNumber = parseInt(prompt("Give me an odd number between 1 and 50 please?"));
+
+function isNumberEven(num) {
+    return num % 2 === 0;
+    // if (num % 2 === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
+
+function isNumberInRange(num) {
+    return num >= 1 && num <= 50;
+}
+
+while(true) {
+    if (!isNumberEven(oddNumber) && isNumberInRange(oddNumber)){
+        break;
+    }
+    oddNumber = parseInt(prompt("Give me an odd number between 1 and 50 please?"));
+}
 
 for (var i = 1; i < 50; i+=2) {
     if (i === oddNumber) {
