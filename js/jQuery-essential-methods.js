014f54054a925515f61fafa003e9b23e9f6b1392
Javier Ruedas <javier@codeup.com>
2021-05-04 15:25:37 -0500

    complete DOM traversal lecture

--- js/jQuery-essential-methods.js
+++ js/jQuery-essential-methods.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     var htmlMethod = $("#leader").html();
 
-    var followerContent = $("#follower").html("I am the leader");
-    console.log(followerContent);
-    followerContent = $("#follower").html();
+    // var followerContent = $("#follower").html("I am the leader");
+    // // console.log(followerContent);
+    // followerContent = $("#follower").html();
 
-    if (followerContent === "I am the leader") {
-        $("#leader").html("What you are is a copycat");
-    }
-    $("#font-changer").click(function(){
-        var currentFontFamily = $("body").css("font-family");
-        //alert(currentFontFamily);
-        if (currentFontFamily === "serif" || currentFontFamily === "Times") {
-            $("body").css("font-family", "sans-serif");
-        } else {
-            $("body").css("font-family", "serif");
-        }
-    });
+    // if (followerContent === "I am the leader") {
+    //     $("#leader").html("What you are is a copycat");
+    // }
+    // $("#font-changer").click(function(){
+    //     var currentFontFamily = $("body").css("font-family");
+    //     //alert(currentFontFamily);
+    //     if (currentFontFamily === "serif" || currentFontFamily === "Times") {
+    //         $("main").css("font-family", "sans-serif");
+    //     } else {
+    //         $("main").css("font-family", "serif");
+    //     }
+    // });
 
     // $("h2").mouseenter(function(){
     //     $("h2").addClass("highlight");
@@ -40,7 +40,45 @@ $(document).ready(function(){
     //     }
     // );
 
-    $("h1").mouseenter(function(){
-        $(this).toggleClass("wildIt");
+    // $("h1").mouseenter(function(){
+    //     $(this).toggleClass("wildIt");
+    // });
+
+
+
+    $("#font-changer").click(function(){
+        $("main").toggleClass("sansSerif");
     });
+
+    // parent()
+    // children()
+    // prev()
+    // next()
+
+
+    $("#getCicero").click(function(){
+        /* output = */ $(this).parent().parent().parent().prev().children("ul").children().first().css("color", "rebeccapurple");
+        // console.log(output);
+    });
+
+    $("#list-style-remover").click(function(){
+        $(this).parent().parent().parent().next().children().css("list-style", "none");
+
+    });
+
+    $("main ul li").each(function(index/*, listItem*/){
+        $(this).css("font-style", "italic");
+        if (index % 2 !==0){
+            $(this).css("background-color", "cornsilk");
+        }
+        // if (index === 0){
+        //     $(this).css("color", "rebeccapurple");
+        // }
+    });
+
+    $("main ul li").first().css("color", "red");
+
+    $("main ul li").last().css("text-decoration", "underline dotted");
+
+
 });
