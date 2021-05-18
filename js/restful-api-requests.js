91822adcf832574adefe4701fdd3c55563453b56
David Stephens <stephens.a.david@gmail.com>
2021-05-18 12:29:41 -0500

    lunch time still need to talk about delete

--- js/restful-api-requests.js
+++ js/restful-api-requests.js
@@ -1,1 +1,87 @@
-fetch("https://fixed-rural-titanoceratops.glitch.me/books").then(resp => resp.json()).then(data => console.log(data));
+let getOptions = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+const getBooks = () => {
+    fetch("https://jungle-enshrined-couch.glitch.me/books")
+        .then(resp => resp.json())
+        .then(books => {
+            console.log(books);
+            let htmlStr = "";
+            for (let book of books) {
+                htmlStr += `<h1>${book.title}</h1><p>by: ${book.author.firstName} ${book.author.lastName}</p>`;
+            }
+            $("#container").html(htmlStr);
+        });
+}
+
+fetch("https://jungle-enshrined-couch.glitch.me/books/1")
+    .then(resp => resp.json())
+    .then(book => console.log(book));
+
+// POST
+let newBook = {
+    "title": "Percy Jackson & The Lightning Thief",
+    "author": {
+        "firstName": "Rick",
+        "lastName": "Riordan"
+    }
+};
+
+let postOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBook)
+}
+
+$("#addPercy").click(() => {
+    fetch("https://jungle-enshrined-couch.glitch.me/books", postOptions)
+        .then(getBooks);
+});
+
+// PUT
+
+let putThis = {
+    "title": "Percy Jackson & The Sea of Monsters",
+    "author": {
+        "firstName": "Rick",
+        "lastName": "Riordan"
+    }
+}
+
+let putOptions = {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(putThis)
+}
+
+fetch("https://jungle-enshrined-couch.glitch.me/books/6", putOptions).then(getBooks);
+
+// PATCH
+
+let patchThis = {
+    "title": "Percy Jackson & The Titan's Curse",
+    "author": {
+        "firstName": "Trick",
+        "lastName": "Triordan"
+    }
+}
+
+let patchOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(patchThis)
+}
+
+fetch("https://jungle-enshrined-couch.glitch.me/books/7", patchOptions).then(getBooks);
+
+getBooks();
